f4debc3ba3e53fdfe490e0068ed95199963ae5e9
WShqwerty <3168923454@qq.com>
2021-09-04 09:26:17 +0800

    commit all

--- sort.c
+++ sort.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define LENTH_A 5
+
+void moop_sort(int a[]);
+
 int main(void){
-    int a[5] = {1,3,2,4,5},i,j,n;
-    printf("%d\n", sizeof(a));
-    for(i=5-1;i>0;i--){
+    int a[LENTH_A] = {1,3,2,4,5};
+    moop_sort(a);
+    system("pause");
+    return 0;
+}
+
+void moop_sort(int a[]){
+    int i,j,n;
+    for(i=LENTH_A-1;i>0;i--){
         for(j=0;j<i;j++){
             if(a[j]<a[j+1]){
                 n = a[j];
@@ -17,6 +27,4 @@ int main(void){
         printf("%d->", a[i]);
     }
     printf("\n");
-    system("pause");
-    return 0;
 }
