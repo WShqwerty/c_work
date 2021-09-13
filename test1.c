add3174b3cbc64e5796088ef4177a4be269c1971
WShqwerty <3168923454@qq.com>
2021-09-13 20:49:54 +0800

    commit all

--- test1.c
+++ test1.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define Max_num 20
+
 int main(void){
-    printf("hello world!\n");
+    int a[Max_num] = {1, 2, 3, 4, 5, 6};
     int i = 6;
+
+    printf("hello world!\n");
+    printf("%d\n", sizeof(a));
     printf("%d\n", i>>1);
     system("pause");
     return 0;
