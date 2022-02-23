fa499d3f72a97bc18bcc264ed08d2576fdb03a19
WShqwerty <3168923454@qq.com>
2022-02-23 13:42:43 +0800

    commit all

--- test2.cpp
+++ test2.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 using namespace std;
 
+
+
 int main(void){
     cout<< "hello world!"<<endl;
+
+    int i, j;
+    for (i = 1; i < 10; i++){
+        for (j = 1; j <= i; j++){
+            cout<<i<<"*"<<j<<"="<<i*j<<"\t";
+        }
+        cout<<endl;
+    }
+    
     return 0;
 }
